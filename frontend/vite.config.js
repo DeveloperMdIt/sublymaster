a3b8aa34b1d4f433d5844604dd66919e700fbb5f
DeveloperMdIt <developer@md-it-solutions.de>
2026-02-05 08:07:17 +0100

    Fix: Added missing proxy to vite.config.js and further hardened backend boot

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     port: 5174,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   }
 })
